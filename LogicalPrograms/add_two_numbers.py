86faa7e9c16d53fe61512324aba7ae956f76caed
sngtmth <sngtmth@gmail.com>
2021-12-15 14:21:14 +0530

    Added:Logical Prgrams

---
+++ LogicalPrograms/add_two_numbers.py
@@ -0,0 +1,13 @@
+
+#This Programs adds two numbers
+def add_num(n1:int,n2:int):
+    # n1=21
+    # n2=20
+
+    #Add two numbers
+    sum=n1+n2
+
+    #Display the sum
+    print('The sum of {0} and {1} is {2}' .format(n1,n2,sum))
+if __name__=='__main__':
+    add_num(2,3)
